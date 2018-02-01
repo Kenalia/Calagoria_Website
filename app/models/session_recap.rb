d396bde676e2e9997daafde32c6d973b74f547a7
Cameron Justice <cameronjustice@protonmail.com>
2018-01-31 20:06:39 -0600

    Fixed DatePicker on new SessionRecap.

--- app/models/session_recap.rb
+++ app/models/session_recap.rb
@@ -1,3 +1,9 @@
 class SessionRecap < ApplicationRecord
     has_many :characters
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.played_on = Date.today if self.new_record?
+    end
 end
